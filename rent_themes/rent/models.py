d32d4f88d56324450a8c4b02997c6a92b78e2cf2
Alessandro Souza <alessandrojsouza@gmail.com>
2022-01-21 11:44:37 -0300

    update model - add phone, address, email client

--- rent_themes/rent/models.py
+++ rent_themes/rent/models.py
@@ -5,10 +5,19 @@ from django.db.models.deletion import CASCADE
 
 class Client(models.Model):
     name = models.CharField(max_length=60, blank=False)
-
+    email = models.CharField(max_length=60, blank=True)
     def __str__(self):
         return self.name
 
+class Phone(models.Model):
+    ddd = models.CharField(max_length=3)
+    number = models.CharField(max_length=10)
+    client = models.ForeignKey('Client', on_delete=models.CASCADE, related_name='phones')
+    
+    def __str__(self):
+        return self.client.name+ ' ' + self.number
+    
+    
 class Theme(models.Model):
     name = models.CharField(max_length=20, blank=False)
     color = models.CharField(max_length=10)
@@ -32,7 +41,19 @@ class Rent(models.Model):
     end_hours = models.CharField(max_length=5, blank=False, null=False)
     client = models.ForeignKey('Client', on_delete=CASCADE, related_name='rents')
     theme = models.ForeignKey('Theme', on_delete=CASCADE, related_name='rents')
+    address = models.OneToOneField('Address', on_delete=models.CASCADE, null=True)
+    
+    def __str__(self):
+        return str(self.date) + ' ' + self.client.name + ' ' + self.theme.name
+    
+class Address(models.Model):
+    street = models.CharField(max_length=60)
+    number = models.IntegerField(null=True)
+    complement = models.CharField(max_length=50)
+    district = models.CharField(max_length=20)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=20, blank=True)
 
     def __str__(self):
-        return str(self.date)
+        return self.street
     
