56ea1e45b5db5671c099ecd03ac2f93396b44dcb
Alessandro Souza <alessandrojsouza@gmail.com>
2022-07-26 10:37:03 -0300

    update ALLOWED_HOSTS in settings.py

--- rent_themes/rent_themes/settings.py
+++ rent_themes/rent_themes/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'django-insecure-j2jp6if781(%%m%e@ekdy_ezv3s!@c_(m%k-8ln6z05@nd06p+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.21.0.227', 'localhost', '127.0.0.1']
 
-
+ 
 # Application definition
 
 INSTALLED_APPS = [
